ece9947865cf42f01f0afe947a33c9c750456085
Dominik Hable <38699487+dominikhable@users.noreply.github.com>
2025-12-03 21:14:22 +0100

    2025 day03

---
+++ AoC2025/Days/Day03.cs
@@ -0,0 +1,43 @@
+﻿namespace AoC2025.Days;
+
+public class Day03 : Day
+{
+    public override object Task1() => GetInputLines().Sum(GetLowJoltage);
+
+    public override object Task2() => GetInputLines().Sum(GetHighJoltage);
+
+    private int GetLowJoltage(string line)
+    {
+        var max = line.Max();
+        var indexMax = line.IndexOf(max);
+
+        if (indexMax == (line.Length - 1))
+        {
+            return int.Parse($"{line[..indexMax].Max()}{max}");
+        }
+
+        return int.Parse($"{max}{line[(indexMax + 1)..].Max()}");
+    }
+
+    private long GetHighJoltage(string line)
+    {
+        var stack = new Stack<char>(12);
+
+        for (int i = 0; i < line.Length; i++)
+        {
+            while (stack.Count > 0 &&
+                   stack.Peek() < line[i] &&
+                   (line.Length - i) > (12 - stack.Count))
+            {
+                stack.Pop();
+            }
+
+            if (stack.Count < 12)
+            {
+                stack.Push(line[i]);
+            }
+        }
+
+        return long.Parse(string.Concat(stack.Reverse()));
+    }
+}
