8af8fd6a20aaefbe021372ec1e9e3e12068d9312
Dominik Hable <38699487+dominikhable@users.noreply.github.com>
2025-12-04 16:10:16 +0100

    2025 day04

---
+++ AoC2025/Days/Day04.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+
+namespace AoC2025.Days;
+
+public class Day04 : Day
+{
+    public override object Task1() => GetRemovables(GetInputPoints('@')).Count;
+
+    public override object Task2()
+    {
+        var rolls = GetInputPoints('@');
+        var initRollCount = rolls.Count;
+
+        while (true)
+        {
+            var removables = GetRemovables(rolls);
+
+            if (removables.Count is 0) break;
+
+            rolls.ExceptWith(removables);
+        }
+
+        return initRollCount - rolls.Count;
+    }
+
+    private static List<Point> GetRemovables(HashSet<Point> rolls)
+    {
+        return [.. rolls.Where(r => CountNeighbors(r, rolls) < 4)];
+    }
+
+    private static int CountNeighbors(Point p, HashSet<Point> grid)
+    {
+        return Util.AdjacentNeighbors.Count(o => grid.Contains(new Point(p.X + o.dx, p.Y + o.dy)));
+    }
+}
