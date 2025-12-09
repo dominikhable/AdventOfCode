b4f2eb2ea37bfb1a93e46358fb20934509a63ad6
Dominik Hable <38699487+dominikhable@users.noreply.github.com>
2025-12-09 16:09:16 +0100

    2025 day09 part1

---
+++ AoC2025/Days/Day09.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+namespace AoC2025.Days;
+
+public class Day09 : Day
+{
+    public override object Task1()
+    {
+        var tiles = GetInputLines()
+            .Select(line => line.Split(','))
+            .Select(parts => (X: long.Parse(parts[0]), Y: long.Parse(parts[1])))
+            .ToList();
+
+        return tiles
+            .SelectMany((t, i) => tiles.Skip(i).Select(u => CalculateArea(t, u)))
+            .Max();
+    }
+
+    private static long CalculateArea((long X, long Y) from, (long X, long Y) to)
+    {
+        return (Math.Abs(from.X - to.X) + 1) * (Math.Abs(from.Y - to.Y) + 1);
+    }
+
+    public override object Task2()
+    {
+        return string.Empty;
+    }
+}
